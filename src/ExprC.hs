08bfc879d81c0b71aa367917f56ffa7a98093bd8
Isaac Elliott <isaace71295@gmail.com>
2022-05-17 21:50:41 +1000

    optimise ExprC

--- src/ExprC.hs
+++ src/ExprC.hs
@@ -32,3 +32,29 @@ showC expr =
     IntC i -> show i
     AddC -> "add"
     MulC -> "mul"
+
+optimise :: ExprC -> ExprC
+optimise expr =
+  case expr of
+    ComposeC f g ->
+      case (optimise f, optimise g) of
+        (IdC, g') -> g'
+        (f', IdC) -> f'
+        (f', g') -> ComposeC f' g'
+    ProductC fs ->
+      let fs' = optimise <$> fs
+          prjs = PrjC <$> reverse [0 .. length fs - 1]
+       in if fs' == prjs
+            then IdC
+            else case fs' of
+              ComposeC g x : fs''
+                | all (\f'' -> case f'' of ComposeC _ x' -> x == x'; _ -> False) fs' ->
+                    optimise $ ComposeC (ProductC $ g : fmap (\(ComposeC g' _) -> g') fs'') x
+              _ -> ProductC fs'
+    AbsC body -> AbsC $ optimise body
+    PrjC {} -> expr
+    IntC {} -> expr
+    IdC -> expr
+    AppC -> expr
+    AddC -> expr
+    MulC -> expr
